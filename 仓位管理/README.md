60263a208bec9d50863600f9f1a4b6d3e432b016
Imagle <wangrq2008@gmail.com>
2019-01-04 16:29:45 +0800

    first ci

---
+++ 仓位管理/README.md
@@ -0,0 +1,13 @@
+# 仓位管理小工具
+
+## 依赖
+    系统需要安装python
+
+## 使用方法
+### Mac or Linux
+打开终端：
+
+    python assets_controller.py 当日净值 5日平均净值
+
+### Windows
+    
---
+++ 仓位管理/assets_controller.py
@@ -0,0 +1,243 @@
+# -*- coding: UTF-8 -*-
+import time
+import sys
+import json
+import urllib2
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+xueqiuHeader = {"Accept": 'application/json, text/javascript, */*; q=0.01',
+                 "Referer": 'https: // xueqiu.com / hq',
+                 "User-Agent": 'Mozilla / 5.0(Macintosh;Intel Mac OS X 10_14_1) AppleWebKit / 537.36(KHTML, like Gecko) \
+                 Chrome / 71.0.3578.98 Safari/537.36',
+                 "X-Requested-With": 'XMLHttpRequest',
+                 "Cookie": '__utmz=1.1521689062.37.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; \
+                 _ga=GA1.2.2022971702.1507098243; device_id=7912987cc1acb87793b6a39ac3f56509; s=ep120643by; \
+                 bid=9f1e4b7c1bf25a9ee495aee5366eb4dd_jo1cumta; remember=1; remember.sig=K4F3faYzmVuqC0iXIERCQf55g2Y;\
+                  xq_a_token=81926db31b817fddd4200e5c29b8aacff4e8b430; xq_a_token.sig=Dz7a60cf3N36a-icWVoXCdYJ-vk; \
+                  xqat=81926db31b817fddd4200e5c29b8aacff4e8b430; xqat.sig=2ZVCpx41OSIUXW6Oq9EO86-a-o0; \
+                  xq_r_token=b93cde8ccea91007a225d2cea6433e7659f1da84; xq_r_token.sig=yjbRh6uluGQ5qyJO2F4qt8SYvgo; \
+                  xq_is_login=1; xq_is_login.sig=J3LxgPVPUzbBg3Kee_PquUfih7Q; u=4381575082; \
+                  u.sig=n24M3k-s04A1Ivqs3HdaHljiKHI; aliyungf_tc=AQAAAJW4SBrybwAA0+YCapOJ5B8iuoe7; \
+                  Hm_lvt_1db88642e346389874251b5a1eded6e3=1544701289,1544851645,1545271068,1546251974; \
+                  _gid=GA1.2.178431788.1546251974; __utma=1.2022971702.1507098243.1544851651.1546251987.59; \
+                  __utmc=1; __utmt=1; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1546252088; __utmb=1.4.10.1546251987'
+                 }
+
+def getNetValueFactor(net_value, net_value_5d):
+    print "Begin to 计算净值因子..."
+    net_value_factor = 0
+    J = net_value - net_value_5d
+    if J > 0.01:
+        net_value_factor = 4
+    elif  J < 0.01 and J > -0.01:
+        net_value_factor = 3
+    elif J < -0.01:
+        net_value_factor = 2
+    print "    Success 计算净值因子，FACTOR[%s]" % net_value_factor
+    return net_value_factor
+
+def getIndexFactor():
+    print "Begin to get 指数 Data..."
+    index_factor = 0
+    print "    Begin to get 上证50 Data..."
+    #上证50(SH:000016)
+    stock = 'SH000016'
+    index_A50 = getIndexData(stock)
+    index_factor += calMa(index_A50)
+    print "    Begin to get 沪深300 Data..."
+    #沪深300, SH000300
+    stock = 'SH000300'
+    index_300 = getIndexData(stock)
+    index_factor += calMa(index_300)
+    print "    Begin to get 中证500 Data..."
+    #中证500(SH:000905)
+    stock = 'SH000905'
+    index_500 = getIndexData(stock)
+    index_factor += calMa(index_300)
+    print "    Begin to get 创业板 Data..."
+    #创业板(SZ:159915)
+    stock = 'SZ159915'
+    index_cyb = getIndexData(stock)
+    index_factor += calMa(index_cyb)
+    print "    Begin to get 上证指数 Data..."
+    #上证指数(SH:000001)
+    stock = 'SH000001'
+    index_001 = getIndexData(stock)
+    index_factor += calMa(index_001)
+    print "    Success 计算指数因子，FACTOR[%s]" % index_factor
+    return index_factor
+
+def calMa(index_data):
+    index_factor = 0
+    # 5日线
+    diff = index_data[1][1] - index_data[0][1]
+    if diff > 0:
+        index_factor += 0.5
+    # 20日线
+    diff = index_data[1][3] - index_data[0][3]
+    if diff > 0:
+        index_factor += 1
+    # 30日线
+    diff = index_data[1][4] - index_data[0][4]
+    if diff > 0:
+        index_factor += 2
+    return index_factor
+
+def getIndexData(stock):
+    now = int(time.time()) * 1000
+    url = '''https://stock.xueqiu.com/v5/stock/chart/kline.json?symbol=%s&begin=%s&period=day&type=before&count=-2&indicator=ma''' % (stock, now)
+    contents = urllib2.urlopen(urllib2.Request(
+        url,
+        headers = xueqiuHeader
+    )).read()
+    contents = json.loads(contents)
+    return contents['data']['item']
+
+def getPercentFactor():
+    print "Begin to get 涨跌幅 Data..."
+    print "    Begin to get ZhangTing Data..."
+    now = int(time.time())
+    url = "https://xueqiu.com/stock/cata/stocklist.json?page=1&size=100&order=desc&orderby=percent&type=11%2C12&_=" + str(now)
+    contents = urllib2.urlopen(urllib2.Request(
+        url,
+        headers = xueqiuHeader
+    )).read()
+    contents = json.loads(contents)
+    #print "    Success for getting ZhangTing Data..."
+    #print "    Begin to analyse ZhangTing Data..."
+    zhangting_num = 0
+    zhangting_factor = 0
+    for stock in contents['stocks']:
+        percent = float(stock['percent'])
+        if percent > 9.9:
+            zhangting_num += 1
+    if zhangting_num > 30:
+        zhangting_factor = 0.5
+    if zhangting_num > 50:
+        zhangting_factor += 0.5
+    if zhangting_num >= 100:
+        zhangting_factor += 0.5
+    if zhangting_num > 200:
+        zhangting_factor += 0.5
+    #print "    Success for analysing ZhangTing Data, NUM[%s], Factor[%s]..." % (zhangting_num, zhangting_factor)
+    print "    Begin to get DieTing Data..."
+    now = int(time.time())
+    url = "https://xueqiu.com/stock/cata/stocklist.json?page=1&size=101&order=asc&orderby=percent&type=11%2C12&_=" + str(now)
+    contents = urllib2.urlopen(urllib2.Request(
+        url,
+        headers = xueqiuHeader
+    )).read()
+    contents = json.loads(contents)
+    #print "    Success for getting DieTing Data..."
+    #print "    Begin to analyse Dieting Data..."
+    dieting_num = 0
+    for stock in contents['stocks']:
+        percent = float(stock['percent'])
+        if percent < -9.9:
+            dieting_num += 1
+    #print "    Success for analysing DieTing Data, Z_NUM[%s], D_NUM[%s]..." % (zhangting_num, dieting_num)
+    if dieting_num > zhangting_num:
+        zhangting_factor += -1
+    print "    Success 计算涨跌幅: 涨停[%s], 跌停[%s], FACTOR[%s]..." % (zhangting_num, dieting_num, zhangting_factor)
+    return zhangting_factor
+
+def getVolumeFactor():
+    print "Begin to get 交易量 Data..."
+    now = int(time.time())
+    url = "https://xueqiu.com/stock/quote_order.json?page=1&size=100&order=desc&exchange=CN&stockType=sha&column=symbol%2Cname%2Ccurrent%2Cchg%2Cpercent%2Clast_close%2Copen%2Chigh%2Clow%2Cvolume%2Camount%2Cmarket_capital%2Cpe_ttm%2Chigh52w%2Clow52w%2Chasexist&orderBy=volume&_=" + str(now)
+    contents = urllib2.urlopen(urllib2.Request(
+        url,
+        headers = xueqiuHeader
+    )).read()
+    contents = json.loads(contents)
+    sha_stocks = contents['data']
+    print "    Success for getting 沪市交易量..."
+
+    url = "https://xueqiu.com/stock/quote_order.json?page=1&size=100&order=desc&exchange=CN&stockType=sza&column=symbol%2Cname%2Ccurrent%2Cchg%2Cpercent%2Clast_close%2Copen%2Chigh%2Clow%2Cvolume%2Camount%2Cmarket_capital%2Cpe_ttm%2Chigh52w%2Clow52w%2Chasexist&orderBy=volume&_=" + str(now)
+    contents = urllib2.urlopen(urllib2.Request(
+        url,
+        headers = xueqiuHeader
+    )).read()
+    contents = json.loads(contents)
+    sza_stocks = contents['data']
+    print "    Success for getting 深市交易量..."
+
+    url = "https://xueqiu.com/stock/quote_order.json?page=1&size=100&order=desc&exchange=CN&stockType=cyb&column=symbol%2Cname%2Ccurrent%2Cchg%2Cpercent%2Clast_close%2Copen%2Chigh%2Clow%2Cvolume%2Camount%2Cmarket_capital%2Cpe_ttm%2Chigh52w%2Clow52w%2Chasexist&orderBy=volume&_=" + str(
+        now)
+    contents = urllib2.urlopen(urllib2.Request(
+        url,
+        headers=xueqiuHeader
+    )).read()
+    contents = json.loads(contents)
+    cyb_stocks = contents['data']
+    print "    Success for getting 创业板交易量..."
+    #print "    Begin to analyse Volume Data..."
+    tmp_list = merge_data(sha_stocks, sza_stocks)
+    final_list = merge_data(tmp_list, cyb_stocks)
+    volume_factor = 0
+    red_num = 0
+    green_num = 0
+    for i, stock in enumerate(final_list):
+        p = float(stock[4])
+        if p > 0:
+            red_num += 1
+        if p < 0:
+            green_num += 1
+        if i is 9 or i is 49 or i is 99 or i is 199:
+            if red_num > green_num:
+                volume_factor += 0.25
+    print "    Success 计算交易量因子, FACTOR[%s]..." % volume_factor
+    return volume_factor
+
+def merge_data(a_list, b_list):
+    tmp_list = []
+    i = 0
+    j = 0
+    len_a = len(a_list)
+    len_b = len(b_list)
+    a_v = a_list[i]
+    b_v = b_list[j]
+    while i < len_a and j < len_b:
+        while( i < len_a and j < len_b and a_v[9] >= b_v[9]) :
+            tmp_list.append(a_v)
+            i += 1
+            if i < len_a:
+                a_v = a_list[i]
+        while (i < len_a and j < len_b and a_v[9] < b_v[9]):
+            tmp_list.append(b_v)
+            j += 1
+            if j < len_b:
+                b_v = b_list[j]
+    while i < len_a:
+        tmp_list.append(a_list[i])
+        i += 1
+    while j < len_b:
+        tmp_list.append(b_list[j])
+        j += 1
+    return tmp_list
+
+def main(net_value_cur, net_value_5d):
+    net_value_factor = getNetValueFactor(net_value_cur, net_value_5d)
+    index_factor = getIndexFactor()
+    zhangting_factor = getPercentFactor()
+    volume_factor = getVolumeFactor()
+    chicang = net_value_factor + index_factor + zhangting_factor + volume_factor
+    print ""
+    print "======前方高能，请注意======\n"
+    print "    目前应该保持的仓位是 [%s]\n" % chicang
+    print "==========祝君好运==========\n"
+
+def userage():
+    print "使用说明："
+    print "    python assets_controller.py 当日净值 5日平均净值"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3 :
+        userage()
+        sys.exit(0)
+    net_value_cur = float(sys.argv[1])
+    net_value_5d = float(sys.argv[2])
+    main(net_value_cur, net_value_5d)
+
+
